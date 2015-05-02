56e020cd5cec1628d85fe3f126c6eaf54341b480
D6C92FE5 <D6C92FE5@hotmail.com>
2015-05-02 22:39:36 +0800

    Tab to space

--- NoninteractiveUnlockCredentialProvider/CPipeListener.h
+++ NoninteractiveUnlockCredentialProvider/CPipeListener.h
@@ -7,17 +7,17 @@
 class CPipeListener
 {
 public:
-	CPipeListener(void);
-	~CPipeListener(void);
-	HRESULT Initialize(CSampleProvider *pProvider);
-	BOOL GetUnlockingStatus();
-	void GetCredential(PWSTR *pwzUsername, PWSTR *pwzPassword);
+    CPipeListener(void);
+    ~CPipeListener(void);
+    HRESULT Initialize(CSampleProvider *pProvider);
+    BOOL GetUnlockingStatus();
+    void GetCredential(PWSTR *pwzUsername, PWSTR *pwzPassword);
 
 private:
-	static DWORD WINAPI _ThreadProc(LPVOID lpParameter);
+    static DWORD WINAPI _ThreadProc(LPVOID lpParameter);
 
-	CSampleProvider  *_pProvider;  // Pointer to our owner.
-	BOOL _fUnlocked;  // Whether or not we're connected.
-	PWSTR _pwzUsername;
-	PWSTR _pwzPassword;
+    CSampleProvider  *_pProvider;  // Pointer to our owner.
+    BOOL _fUnlocked;  // Whether or not we're connected.
+    PWSTR _pwzUsername;
+    PWSTR _pwzPassword;
 };
